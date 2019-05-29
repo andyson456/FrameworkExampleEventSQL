dc24d77f14057002fbf3140817a2e1091f515e8b
Andrew Larson <andyson456@gmail.com>
2019-05-29 15:03:55 -0700

    Starting to write tests for Product class

---
+++ EventTestClasses/ProductTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+using EventClasses;
+using EventPropsClasses;
+using EventDBClasses;
+using ToolsCSharp;
+
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+using System.Data;
+using System.Data.SqlClient;
+
+using DBCommand = System.Data.SqlClient.SqlCommand;
+
+namespace EventTestClasses
+{
+	[TestFixture]
+	public class ProductTests
+	{
+		private string dataSource = "Data Source=LAPTOP-VOARD1KV\\SQLEXPRESS;Initial Catalog=MMABooksUpdated;Integrated Security=True";
+
+		[SetUp]
+		public void TestResetDatabase()
+		{
+			CustomerSQLDB db = new CustomerSQLDB(dataSource);
+			DBCommand command = new DBCommand();
+			command.CommandText = "usp_testingResetData";
+			command.CommandType = CommandType.StoredProcedure;
+			db.RunNonQueryProcedure(command);
+		}
+
+		[Test]
+		public void TestNewEventConstructor()
+		{
+			// not in Data Store - no id
+			Product prod = new Product(dataSource);
+			Console.WriteLine(prod.ToString());
+			Assert.Greater(prod.ToString().Length, 1);
+		}
+	}
+}
